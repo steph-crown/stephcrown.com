fa9d35532f2de6950bade5178b4e42aa98759636
Steph Crown <emmanuelstephen024@gmail.com>
2023-12-03 21:37:15 +0100

    Turn logo to home link

--- src/Components/DpNameTitle/DpNameTitle.tsx
+++ src/Components/DpNameTitle/DpNameTitle.tsx
@@ -1,15 +1,17 @@
+import { APP_ROUTES } from 'Constants'
+import { Link } from 'react-router-dom'
 import React from 'react'
 import { stephcrown } from 'Assets/Pngs'
 
 const DpNameTitle = () => {
   return (
-    <div className='flex items-center '>
+    <Link className='flex items-center ' to={APP_ROUTES.Home}>
       <img src={stephcrown} alt='Steph Crown' className='h-10' />
       <div className='ml-4'>
         <p className='font-MSMediumWide leading-[160%] dark:text-fg/100-dark text-fg/100-light  text-sm xs:text-base'>Stephen Emmanuel</p>
         <p className='leading-[160%] text-fg/50-light dark:text-fg/50-dark font-MSMedium text-xs xs:text-sm'>Software Engineer</p>
       </div>
-    </div>
+    </Link>
   )
 }
 
