dc26debb76e75f313f72705eda9d2151b0124022
Steph Crown <emmanuelstephen024@gmail.com>
2024-04-11 22:26:48 +0100

    Add languages tools and frameworks

--- src/Components/PdfViewer/PdfViewer.tsx
+++ src/Components/PdfViewer/PdfViewer.tsx
@@ -4,7 +4,7 @@ const PdfViewer = () => {
   return (
     <div>
       <div className='flex justify-end w-full mb-4'>
-        <a href='/Emmanuel_Stephen_Resume.pdf' download={true}>
+        <a href='./Emmanuel_Stephen_Resume.pdf' download={true}>
           <button className='btn'>
             Click to download <DownloadIcon />
           </button>
@@ -13,7 +13,7 @@ const PdfViewer = () => {
       <div className='h-[calc(100vh_-_14rem)] overflow-y-auto border-nav border rounded-lg'>
         <iframe
           style={{ width: '100%', height: '100%' }}
-          src='https://drive.google.com/file/d/1QAnAKZIsmKnvjslyWMV39GDYHWPZUX-e/preview'
+          src='https://drive.google.com/file/d/1umBKU-dM0Gk_63a-XzosZFyvPdcL4I6h/preview'
           width='640'
           height='480'
           allow='autoplay'
--- src/Pages/Tools/Tools.tsx
+++ src/Pages/Tools/Tools.tsx
@@ -1,26 +1,98 @@
 import { ToolIcon } from 'Assets/Svgs'
 
-const Home = () => {
+const tools = [
+  'HTML',
+  'CSS',
+  'Styled Components',
+  'JavaScript',
+  'TypeScript',
+  'Angular',
+  'React.js',
+  'Redux',
+  'Redux Toolkit',
+  'Formik',
+  'React Hook Form',
+  'Next.js',
+  'Jest',
+  'React Testing Library',
+  'TailwindCSS',
+  'ChakraUI',
+  'Storybook',
+  'Material UI',
+  'D3.js',
+  'jQuery',
+  'Axios',
+  'PWA',
+  'Yup',
+  'GSAP',
+  'GraphQL',
+  'Webpack',
+  'Babel',
+  'Lighthouse',
+  'Web Components',
+  'Stencil.js',
+  'Elixir',
+  'OTP',
+  'Phoenix',
+  'Phoenix LiveView',
+  'PostgreSQL',
+  'Ecto',
+  'Mix',
+  'ExUnit',
+  'Node.js',
+  'Python',
+  'Bash',
+  'Docker',
+  'Git',
+  'GitHub',
+  'GitHub Actions',
+  'GitHub Workflows',
+  'NPM',
+  'VS Code',
+  'Figma',
+  'ESLint',
+  'Notion',
+  'Jira',
+  'Markdown',
+  'ClojureScript',
+  'GraphQL',
+]
+
+const Tools = () => {
   return (
     <div>
       <div className='mb-10'>
         <ToolIcon className='h-[4.75rem] w-[4.75rem] text-fg/50-light dark:text-fg/50-dark' />
 
-        <p className='text-fg/100-light dark:text-fg/100-dark font-MSMediumWide'>Undergoing Construction ..</p>
-        {/* <article>
+        {/* <p className='text-fg/100-light dark:text-fg/100-dark font-MSMediumWide'>Undergoing Construction ..</p> */}
+        <article>
           <h1 className='my-2 h1'>
-            <span className='h1-alt'>Languages, tools, and frameworks</span>
-            <br />I work with<span className='h1-alt'>.</span>
+            <span className='h1-alt'>Languages, tools, </span>
+            <br />
+            and frameworks<span className='h1-alt'>.</span>
           </h1>
 
           <p className='p'>
             Here&apos;s a list of some of the languages, tools, and frameworks I work with. I&apos;m always learning new things and I&apos;m open to
             new ideas and technologies.
           </p>
-        </article> */}
+
+          <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-4'>
+            {/* Map through the 'tools' array to render each tool */}
+            {tools.map((tool, index) => (
+              <div
+                key={tool}
+                className='bg-bg/5-light dark:bg-bg/5-dark p-4 rounded-md text-center
+                       text-fg/80-light dark:text-fg/80-dark hover:bg-gray-200 dark:hover:bg-gray-700'
+              >
+                {tool}
+              </div>
+            ))}
+          </div>
+        </article>
       </div>
     </div>
   )
 }
 
-export default Home
+export default Tools
