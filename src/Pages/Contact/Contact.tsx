7de3a1ad0311f449cbbe023ee297551f9b4408d3
Steph Crown <emmanuelstephen024@gmail.com>
2024-10-03 10:11:20 +0100

    Add updates

--- src/Pages/Contact/Contact.tsx
+++ src/Pages/Contact/Contact.tsx
@@ -21,7 +21,7 @@ const Contact = () => {
         </article>
       </div>
 
-      <form className='form' name='contactForm' method='POST' data-netlify='true' data-netlify-honeypot='true'>
+      <form className='form' name='contactForm' method='POST' data-netlify='true' id='contactForm'>
         <div className='flex gap-6 md:gap-10 flex-col lg:flex-row'>
           <label className='label'>
             Name: <input type='text' className='input' placeholder='Enter your name' name='name' />
